9a6b5ecdb89cf244148f26dc219b4cb2bd7b22d6
Andrey Vladikin <vladikin@gmail.com>
2025-12-23 11:36:11 +0200

    deploy server 7

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -36,5 +36,5 @@ console.log(
 
 const PORT = process.env.PORT || 3000;
 httpServer.listen(PORT, () => {
-  console.log(`🚀 Server is running on port ${PORT}.`);
+  console.log(`🚀 Server is running on port ${PORT}`);
 });

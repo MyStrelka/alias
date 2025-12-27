a9b4f73829c89c27de3e98a327f5fad61ef27313
Andrey Vladikin <vladikin@gmail.com>
2025-12-27 13:19:33 +0200

    server: remove unused env vars

--- apps/server/src/config.ts
+++ apps/server/src/config.ts
@@ -14,16 +14,6 @@ if (
   );
 }
 
-// export const FirebaseConfig = {
-//   apiKey: process.env.FIREBASE_apiKey,
-//   authDomain: process.env.FIREBASE_authDomain,
-//   projectId: process.env.FIREBASE_projectId,
-//   storageBucket: process.env.FIREBASE_storageBucket,
-//   messagingSenderId: process.env.FIREBASE_messagingSenderId,
-//   appId: process.env.FIREBASE_appId,
-//   measurementId: process.env.FIREBASE_measurementId,
-// };
-
 export const DiscordAuthConfig = {
   authorizationURL: process.env.OAUTH_DISCORD_URL || '',
   clientID: process.env.OAUTH_DISCORD_CLIENT_ID || '',
